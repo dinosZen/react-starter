7ba82045caefeb6cfe8bd28435fff12bf3629469
Dino Santic <dino@zendev.se>
2025-04-22 18:34:57 +0200

    Added ErrorPage and route LoadingFallback component.

---
+++ src/components/LoadingFallback.tsx
@@ -0,0 +1,7 @@
+const LoadingFallback = () => (
+  <div className="flex items-center justify-center min-h-screen">
+    <div className="animate-spin rounded-full h-18 w-18 border-t-2 border-b-2 border-background-brand-default"></div>
+  </div>
+);
+
+export default LoadingFallback;
